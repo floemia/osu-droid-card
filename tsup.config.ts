47461d3e7a6ca63782a40cfdfc3958af45af285a
Floemia <mcescobar73@gmail.com>
2024-11-17 15:41:56 -0300

    first commit!

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig(({ watch = false }) => ({
   clean: true,
   target: "es2022",
-  //dts: true,
+  dts: true,
   sourcemap: true,
   entry: {
     index: "src/index.ts",
-	test: "test/index.ts"
+	//test: "test/index.ts"
   },
   external: [],
   format: ["cjs", "esm"],
